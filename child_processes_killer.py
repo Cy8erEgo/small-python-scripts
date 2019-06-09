7ad0000defa9ad15bd40437d7838ee8838f1e3bc
verysick <verysick6666@yandex.ru>
2019-06-10 01:08:32 +0700

    Init

---
+++ child_processes_killer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import subprocess 
+import psutil
+import os
+import time
+
+
+def create_child_processes():
+    sites = ['google.com', 'yandex.ru', 'yahoo.com']
+
+    for site in sites:
+        proc = subprocess.Popen(['ping', site])
+
+        print('[+] Created child process with pid', proc.pid)
+
+def kill_child_processes():
+    cur_proc = psutil.Process(os.getpid())
+
+    for child_proc in cur_proc.children(): 
+        child_proc.kill()
+
+        print('[-] Killed procecc with pid', child_proc.pid)
+
+
+if __name__ == '__main__':
+    create_child_processes()
+    print()
+    time.sleep(15)
+    print()
+    kill_child_processes()
+
+
